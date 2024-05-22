a6beb2f3d6da1079f29de4985cb31c691d1eb036
Pablo Martin Morente <politotuto@gmail.com>
2024-05-22 13:28:41 +0200

    mouse leave casi arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,64 @@
-import { useState } from 'react'
+import { useState, useRef,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+let animationPerforming=false
 function App() {
   const [xPerspective, setXperspective] = useState('0deg')
-
+  const bgVideo = useRef(null);
   const [timeoutId, setTimeoutId] = useState(null);
   const [yPerspective, setYperspective] = useState('0deg')
+  let [currentFace,setCurrentFace]=useState(0)
   let conf = {
     maxDeg: 4
   }
+  useEffect(()=>{
+    const videoElement = bgVideo.current;
+    if (videoElement) {
+      videoElement.muted = true;
+      videoElement.play()
+        .catch(error => {
+          console.error('Error attempting to play video:', error);
+        });
+    }
+  },[])
+  let FrontFace=()=>(
+  <div className='leftSide'>
+  <img src="../avatar.png" alt="" draggable='false' />
+  <div className='info'>
+    <h2>Pablo Martín</h2>
+    <h3>Full Stack Developer</h3>
+    <div className='flags'>
+      <img src="../public/spflag.webp" alt="" />
+      <img src="../public/ukflag.png" alt="" />
+    </div>
+    <div className="buttons">                  
+    <button>My stack & projects</button>
+    <button>More about me</button>
+    </div>
+  </div>
+</div>)
+let BackFace=()=>(
+  <div className='leftSide'>
+  <img src="/avatar.png" alt="" draggable='false' />
+  <div className='info'>
+    <h2>Pablo Martín</h2>
+    <h3>Full Stack Developer</h3>
+  </div>
+</div>
+)
   return (
     <>
-      <video src="../public/Sin título.mp4" className='backgroundVideo' onCanPlay={(e) => {
-        e.currentTarget.play()
-      }}
+      <video src="/public/Sin título.mp4" className='backgroundVideo' ref={bgVideo}
         onEnded={(e) => {
           e.currentTarget.currentTime = 0;
           e.currentTarget.play()
         }}
       ></video>
-
       <div className="banner">
         <div className='card' onMouseMove={cardMouseMove}
           onMouseLeave={(e) => {
             let currentTarget = e.currentTarget
+            console.log('currentTarget: ', currentTarget);
             setTimeout(() => {
               currentTarget.children[0].classList.add('animation')
               setTimeoutId(setTimeout(() => {
@@ -34,24 +69,38 @@ function App() {
                 }, 50);
               }, 1000));
             }, 2);
-          }}>
-          <div className="content" style={{ '--xPerspective': xPerspective, '--yPerspective': yPerspective }}>
-            <div className='leftSide'>
-              <img src="../avatar.png" alt="" draggable='false' />
-              <div className='info'>
-                <h2>Pablo Martín</h2>
-                <h3>Full Stack Developer</h3>
-                <div className='flags'>
-                  <img src="../public/spflag.webp" alt="" />
-                  <img src="../public/ukflag.png" alt="" />
-                </div>
-                <div className="buttons">                  
-                <button>My stack & projects</button>
-                <button>More about me</button>
-                </div>
-              </div>
-            </div>
-            <div></div>
+          }}
+          onClick={(e)=>{
+            if(!animationPerforming){
+              animationPerforming=true
+              let currentTarget=e.currentTarget
+            e.currentTarget.style.animation='flipLeftSide 0.5s forwards linear'
+            setTimeout(() => {
+              currentFace==0 ?setCurrentFace(1):setCurrentFace(0)  
+           currentTarget.style.animation='flipRightSide 0.5s forwards linear'
+           setTimeout(() => {
+            animationPerforming=false
+           }, 500);
+            }, 500);
+            }
+          }}
+          >
+          <div className={`content ${currentFace == 0 ? "frontFace" : "backFace"}`} style={{ '--xPerspective': xPerspective, '--yPerspective': yPerspective }}>
+          <div className='leftSide'>
+  <img src="../avatar.png" alt="" draggable='false' />
+  <div className='info'>
+    <h2>Pablo Martín</h2>
+    <h3>Full Stack Developer</h3>
+    <div className='flags'>
+      <img src="../public/spflag.webp" alt="" />
+      <img src="../public/ukflag.png" alt="" />
+    </div>
+    <div className="buttons">                  
+    <button>My stack & projects</button>
+    <button>More about me</button>
+    </div>
+  </div>
+</div>
           </div>
         </div>
       </div>
